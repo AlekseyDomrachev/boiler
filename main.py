6c78c8d552e4bed43f6cc0dc883e8f8957a6e550
AlekseyDomrachev <89600926823@mail.ru>
2022-09-02 13:47:30 +0300

    Запись в отдельном потоке

--- main.py
+++ main.py
@@ -2,33 +2,27 @@ from modbus import *
 
 # mbr = Read()
 # mbr.start()
-# mbw = Write(MB_DATA)
-# mbw.start()
+mbw = Write(MB_READ_DATA, MB_DATA)
+mbw.start()
+
 while True:
-    t1 = time.time()
-# MB_DATA = server.data_bank.get_holding_registers(0, data_size)# Это операция присвоения а не само чтение!!! и в случае изменения там в любой
-#момент времени оно поменяется в любом месте программы!
-#Нужно сделать так:
-# MB_DATA = list(server.data_bank.get_holding_registers(0, data_size))
-#А лучше всего так:
-    if MB_DATA_OLD != server.data_bank.get_holding_registers(0, data_size):# Но в этом случае, при изменении в программе MB_DATA тут сработает и затрет!!!
-        MB_DATA_OLD = server.data_bank.get_holding_registers(0, data_size)
+    if MB_READ_DATA != server.data_bank.get_holding_registers(0, DATA_SIZE):# Но в этом случае, при изменении в программе MB_DATA тут сработает и затрет!!!
+        MB_READ_DATA = server.data_bank.get_holding_registers(0, DATA_SIZE)
         MB_COMPLEAT = True
 
     if MB_COMPLEAT:
-        MB_DATA = server.data_bank.get_holding_registers(0, data_size)
+        MB_DATA = server.data_bank.get_holding_registers(0, DATA_SIZE)
         MB_COMPLEAT = False
 
-    if MB_DATA[0] == 1:
-        qw = 0
-        for q in MB_DATA:
-            if qw != 0:
-                MB_DATA[qw] +=1
-            qw += 1
 
-    if MB_DATA != MB_DATA_OLD:
-        counter = 0
-        for data_to_send in MB_DATA:
-            if data_to_send != MB_DATA_OLD[counter]:
-                server.data_bank.set_holding_registers(counter, [data_to_send])
-            counter += 1
+
+    if MB_DATA[0] == 1:
+        MB_DATA[2] += 1
+
+
+
+
+
+    if MB_DATA != MB_READ_DATA:
+        mbw.data = MB_DATA
+        mbw.data_old = MB_READ_DATA
--- modbus.py
+++ modbus.py
@@ -1,19 +1,19 @@
 from pyModbusTCP.server import ModbusServer
 from threading import Thread
-import time
+# import time
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 ip = s.getsockname()[0]
-# ip = '192.168.1.105'
 server = ModbusServer(ip, 502, no_block=True)
-MB_DATA = []
-MB_DATA_OLD = 0
+MB_DATA = 0
+MB_READ_DATA = 0
 MB_COMPLEAT = False
-data_size = 32000
-while len(MB_DATA) < data_size:
-    MB_DATA.append(0)
+DATA_SIZE = 32000
+
+# while len(MB_DATA) < data_size:
+#     MB_DATA.append(0)
 
 print('Starting server...')
 server.start()
@@ -34,23 +34,32 @@ class Read(Thread):
                 if self.value != server.data_bank.get_holding_registers(0, data_size):
                     self.value = server.data_bank.get_holding_registers(0, data_size)
                     self.read_complit = True
-            time.sleep(self.period)
+            # time.sleep(self.period)
 
 
 class Write(Thread):
-    def __init__(self, data, period=0.1):
+    def __init__(self, data_old, data):
         super(Write, self).__init__()
-        self.period = period
+        # self.period = address
         self.data = data
+        self.data_old = data_old
         # self.value = 0
         # self.read_complit = False
-        self.data_old = 0
+        # self.data_old = 0
         # self.value_old = 0
 
     def run(self):
         while True:
             if server.is_run:
+                # print(self.data_old)
+                # print(type(self.data_old))
+                if self.data != self.data_old and type(self.data_old) is list and type(self.data) is list:
+                    counter = 0
+                    for data_to_send in self.data:
+                        if data_to_send != self.data_old[counter]:
+                            server.data_bank.set_holding_registers(counter, [data_to_send])
+                        counter += 1
                 # if self.data != self.data_old:
-                server.data_bank.set_holding_registers(0, self.data)
+                # server.data_bank.set_holding_registers(0, self.data)
                 # self.data_old = self.data
-            time.sleep(self.period)
+            # time.sleep(self.period)
