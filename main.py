938cfa840fcfb47a66a3a9760ffb968a212ed03b
AlekseyDomrachev <89600926823@mail.ru>
2022-09-02 10:31:32 +0300

    Здесь все работает и чтение и запись и не залипает. Но нет присвоения переменных программы

--- main.py
+++ main.py
@@ -1,13 +1,64 @@
 from modbus import *
-import copy
 
-mb1 = Mb(MB_DATA)
-mb1.start()
+
+# mbr = Read()
+# mbr.start()
+# mbw = Write(MB_DATA)
+# mbw.start()
 r = 0
-while True:
+t1 = 0
+t2 = 0
+r1 = 0
+# while True:
     #Читать нужно, когда данные готовы
-    # if mb1.read_complit:
-    MB_DATA = list(mb1.value)
+    # Определим время записи MB
+
+# t1 = time.time()
+# server.data_bank.set_holding_registers(0, MB_DATA)
+# t2 = time.time()
+# print(t1)
+# print(t2)
+while True:
+    t1 = time.time()
+# MB_DATA = server.data_bank.get_holding_registers(0, data_size)# Это операция присвоения а не само чтение!!! и в случае изменения там в любой
+#момент времени оно поменяется в любом месте программы!
+#Нужно сделать так:
+# MB_DATA = list(server.data_bank.get_holding_registers(0, data_size))
+#А лучше всего так:
+    if MB_DATA_OLD != server.data_bank.get_holding_registers(0, data_size):# Но в этом случае, при изменении в программе MB_DATA тут сработает и затрет!!!
+        MB_DATA_OLD = server.data_bank.get_holding_registers(0, data_size)
+        MB_COMPLEAT = True
+
+
+
+    if MB_COMPLEAT:
+        MB_DATA = server.data_bank.get_holding_registers(0, data_size)
+        MB_COMPLEAT = False
+
+    print(id(MB_DATA), id(MB_DATA_OLD), id(server.data_bank.get_holding_registers(0, data_size)))
+
+    # t2 = time.time()
+    # print(t2-t1, r1)
+    # MB_DATA[2] += 1
+    # print(MB_DATA)
+
+    if MB_DATA != MB_DATA_OLD:
+        server.data_bank.set_holding_registers(0, MB_DATA)
+
+
+
+
+
+
+
+# print(t2)
+
+    # if MB_DATA != server.data_bank.get_holding_registers(0, data_size):
+    #     MB_DATA = server.data_bank.get_holding_registers(0, data_size)
+    #     print('read_compleate', r)
+    #     r += 1
+    # if mbr.read_complit:
+    #     MB_DATA = mbr.value
     #     mb1.read_complit = False
     #     print(MB_DATA[0])
     # if not mb1.is_alive():
@@ -16,9 +67,10 @@ while True:
     # print(MB_DATA)
         # mb1.start()
     # mb1.join(0.1)
-    if MB_DATA[0] == 1:
-        MB_DATA[2] += 1
+    # if MB_DATA[0] == 1:
+    #     MB_DATA[2] += 1
     # if mb1.value[0] == 1:
+    # print(MB_DATA)
     #     MB_DATA[1] = 55
     # else:
     #     MB_DATA[1] = 0
@@ -31,9 +83,14 @@ while True:
 
     # print(MB_DATA[0], MB_DATA[2])
     # print(mb1.value)
-    time.sleep(1)
+    # time.sleep(0.1)
 
     # if MB_DATA != MB_DATA_OLD:
     #     mb1.data = list(MB_DATA)
     # print(id(mb1.data), id(MB_DATA), id(mb1.value))
+    # mbw.data = MB_DATA
+    # r += 1
+    # if M/TA_OLD = list(MB_DATA)
+    # print(MB_DATA)
+
     # r += 1
