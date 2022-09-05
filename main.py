4128d0f2b1afde25ac55561a4890ecb629822740
AlekseyDomrachev <107096596+AlekseyDomrachev@users.noreply.github.com>
2022-09-05 09:34:08 +0300

    В этом варианте чтение и запись осуществляется в розных потоках. Скорость основной программы наивысшая. Но иногда происходит "залипание" кнопки

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from modbus import *
 
-# mbr = Read()
-# mbr.start()
+mbr = Read()
+mbr.start()
 mbw = Write(MB_READ_DATA, MB_DATA)
 mbw.start()
 
 while True:
-    if MB_READ_DATA != server.data_bank.get_holding_registers(0, DATA_SIZE):# Но в этом случае, при изменении в программе MB_DATA тут сработает и затрет!!!
-        MB_READ_DATA = server.data_bank.get_holding_registers(0, DATA_SIZE)
-        MB_COMPLEAT = True
+    # if MB_READ_DATA != server.data_bank.get_holding_registers(0, DATA_SIZE):
+    #     MB_READ_DATA = server.data_bank.get_holding_registers(0, DATA_SIZE)
+    #     MB_COMPLEAT = True
 
-    if MB_COMPLEAT:
-        MB_DATA = server.data_bank.get_holding_registers(0, DATA_SIZE)
-        MB_COMPLEAT = False
+    # if MB_COMPLEAT:
+    #     MB_DATA = server.data_bank.get_holding_registers(0, DATA_SIZE)
+    #     MB_COMPLEAT = False
+    MB_READ_DATA = mbr.data_old
+    MB_DATA = mbr.data
 
-
-
+    # ----------------------------------------------------------------------------
     if MB_DATA[0] == 1:
         MB_DATA[2] += 1
 
 
-
-
-
+    # ----------------------------------------------------------------------------
     if MB_DATA != MB_READ_DATA:
-        mbw.data = MB_DATA
         mbw.data_old = MB_READ_DATA
+        mbw.data = MB_DATA
+    else:
+        mbw.data_old = MB_DATA
+        mbw.data = MB_DATA
+        # print(MB_DATA, MB_READ_DATA)
--- modbus.py
+++ modbus.py
@@ -9,7 +9,7 @@ ip = s.getsockname()[0]
 server = ModbusServer(ip, 502, no_block=True)
 MB_DATA = 0
 MB_READ_DATA = 0
-MB_COMPLEAT = False
+# MB_COMPLEAT = False
 DATA_SIZE = 32000
 
 # while len(MB_DATA) < data_size:
@@ -26,14 +26,19 @@ class Read(Thread):
         super(Read, self).__init__()
         self.period = period
         self.read_complit = False
-        self.value = 0
+        self.data_old = 0
+        self.data = 0
 
     def run(self):
         while True:
             if server.is_run:
-                if self.value != server.data_bank.get_holding_registers(0, data_size):
-                    self.value = server.data_bank.get_holding_registers(0, data_size)
+                if self.data_old != server.data_bank.get_holding_registers(0, DATA_SIZE):
+                    self.data_old = server.data_bank.get_holding_registers(0, DATA_SIZE)
                     self.read_complit = True
+                if self.read_complit:
+                    self.data = server.data_bank.get_holding_registers(0, DATA_SIZE)
+                    self.read_complit = False
+
             # time.sleep(self.period)
 
 
@@ -41,22 +46,17 @@ class Write(Thread):
     def __init__(self, data_old, data):
         super(Write, self).__init__()
         # self.period = address
-        self.data = data
         self.data_old = data_old
-        # self.value = 0
-        # self.read_complit = False
-        # self.data_old = 0
-        # self.value_old = 0
+        self.data = data
 
     def run(self):
         while True:
             if server.is_run:
-                # print(self.data_old)
-                # print(type(self.data_old))
                 if self.data != self.data_old and type(self.data_old) is list and type(self.data) is list:
                     counter = 0
                     for data_to_send in self.data:
                         if data_to_send != self.data_old[counter]:
+                            # print(data_to_send, self.data_old[counter])
                             server.data_bank.set_holding_registers(counter, [data_to_send])
                         counter += 1
                 # if self.data != self.data_old:
